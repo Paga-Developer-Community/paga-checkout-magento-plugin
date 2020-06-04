ee7c51446c8ab4a198a9e1787d7ab221ffbfe9ba
nnamdi16 <nwabuokeinnamdi@gmail.com>
2020-06-04 01:04:45 +0100

    Modified the registration.php file, changing the plugin's name

--- registration.php
+++ registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Paga_ExpressCheckout',
+    'Magento_PagaCheckout',
     __DIR__
 );
