5d0116b491a525cd1bd908efb5b6ef1a304f246d
nnamdi16 <nwabuokeinnamdi@gmail.com>
2020-06-09 15:31:47 +0100

    Modified opening braces position.

--- Controller/Checkout/Refresh.php
+++ Controller/Checkout/Refresh.php
@@ -5,11 +5,13 @@ namespace Magento\PagaCheckout\Controller\Checkout;
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
 
-class Refresh extends Action {
+class Refresh extends Action
+{
 
-    public function execute() {
+    public function execute()
+    {
         return $this->getResponse()->representJson(
-                        $this->_objectManager->get('Magento\Framework\Json\Helper\Data')->jsonEncode(['success' => true])
+            $this->_objectManager->get('Magento\Framework\Json\Helper\Data')->jsonEncode(['success' => true])
         );
     }
 
